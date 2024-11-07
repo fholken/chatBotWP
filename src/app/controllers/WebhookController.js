3b784b08b0ffa54ebd9bae4eea06bdb410724e0c
Mauricio <mauricio.gomez@serviunix.com>
2024-11-07 14:46:56 -0500

    feature/implementado_menu_interactivo

--- src/app/controllers/WebhookController.js
+++ src/app/controllers/WebhookController.js
@@ -4,13 +4,28 @@ import { config } from '../../config/environment.js';
 const messagingService = new MessagingService();
 
 export class WebhookController {
+
+  constructor() {
+    this.appointmentState = {};
+  }
   async handleWebhookPost(req, res) {
     console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
 
     const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+    const senderInfo = req.body.entry?.[0]?.changes[0]?.value?.contacts?.[0];
     if (message?.type === "text") {
-      await messagingService.replyToMessage(message); // Replicar el mensaje de texto a través de WhatsApp
-      await messagingService.markMessageAsRead(message.id); // Marcar el mensaje como leído en WhatsApp
+      const incomingMessage = message.text.body.toLowerCase().trim();
+      if (this.isGreeting(incomingMessage)) {
+        await this.sendWelcomeMessage(message.from, message.id, senderInfo);
+        await this.sendWelcomeMenu(message.from);
+      } 
+      // else {
+      //   await messagingService.replyToMessage(message); // Replicar el mensaje de texto a través de WhatsApp
+      // }
+      await messagingService.markMessageAsRead(message.id); // Marcar el mensaje como leído en WhatsApp        
+    } else if (message?.type === "interactive") {
+      const opcion = message?.interactive?.button_reply?.id;
+      await this.handleMenuOption(message.from, opcion);
     }
     res.sendStatus(200);
   }
@@ -27,4 +42,48 @@ export class WebhookController {
       res.sendStatus(403);
     }
   }
+
+  isGreeting(message) {
+    const gretings = ["hola", "buenos dias", "buenas tardes", "buenas noches", "hoy es un buen dia", "buenos dias"];
+    return gretings.includes(message);
+  }
+
+  getSenderName(senderInfo) {
+    return senderInfo?.profile?.name?.split(" ")[0] || senderInfo.wa_id || "";
+  }
+  async sendWelcomeMessage(to, messageId, senderInfo) {
+    const name = this.getSenderName(senderInfo);
+    const welcomeMessage = `Hola ${name}, Bienvenido a Una cosa de locos.`;
+    await messagingService.sendMessage(to, welcomeMessage, messageId);
+  }
+
+  async sendWelcomeMenu(to) {
+    const menuMessage = `Elija una opción:`;
+    const buttons = [
+      { type: "reply", reply: { id: "option_1", title: "Comprar" } },
+      { type: "reply", reply: { id: "option_2", title: "Consultar" } },
+      { type: "reply", reply: { id: "option_3", title: "Distribuidores" } },
+    ];
+    await messagingService.sendInteractiveButtons(to, menuMessage, buttons);
+  }
+
+  async handleMenuOption(to, option) {
+    let response;
+    switch (option) {
+      case "option_1":
+        response = "Veo que estas interesado en comprar";
+        break;
+      case "option_2":
+        response = "Veo que estas interesado en consultar";
+        break;
+      case "option_3":
+        response = "Veo que estas interesado en consultar distribuidores";
+        break;
+      default:
+        response = "Lo siento no te entendí tu selección";
+        break;
+    }
+    await messagingService.sendMessage(to, response);
+  }
+
 }
--- src/app/services/MessagingService.js
+++ src/app/services/MessagingService.js
@@ -3,14 +3,14 @@ import { axiosClient } from '../../infrastructure/axiosClient.js';
 export class MessagingService {
 
   // Replicar el mensaje de texto a través de WhatsApp
-  async replyToMessage(message) {
-    return await axiosClient.post('/messages', {
-      messaging_product: 'whatsapp',
-      to: message.from,
-      text: { body: 'Echo: ' + message.text.body },
-      context: { message_id: message.id },
-    });
-  }
+  // async replyToMessage(message) {
+  //   return await axiosClient.post('/messages', {
+  //     messaging_product: 'whatsapp',
+  //     to: message.from,
+  //     text: { body: 'Echo: ' + message.text.body },
+  //     context: { message_id: message.id },
+  //   });
+  // }
 
   // Marcar el mensaje como leído en WhatsApp
   async markMessageAsRead(messageId) {
@@ -18,6 +18,33 @@ export class MessagingService {
       messaging_product: 'whatsapp',
       status: 'read',
       message_id: messageId,
+    });
+  }
+
+  async sendMessage(to, message) {
+    const data = {
+      messaging_product: 'whatsapp',
+      to: to,
+      text: { body: message },
+    };   
+
+    return await axiosClient.post('/messages', data);
+  }
+
+  async sendInteractiveButtons(to, BodyText, buttons) {
+    return await axiosClient.post('/messages', {
+      messaging_product: 'whatsapp',
+      to: to,
+      type: 'interactive',
+      interactive: {
+        type: 'button',
+        body: {
+          text: BodyText,
+        },
+        action: {
+          buttons: buttons,
+        },
+      },
     });
   }
 }
